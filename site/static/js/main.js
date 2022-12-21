cdd48e97f76a8da7166f3056c01d1e0d5e7e29f5
akabbauti <bautistaigarzabal11@gmail.com>
2022-12-21 02:04:28 -0300

    js comments

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -1,115 +1,123 @@
-//  En un futuro agregar sistema de puntos
+// Variables
 
-// const puntosStorage = localStorage.getItem('puntos');
-
-// let puntos = parseInt(puntosStorage)
-
-// if(puntosStorage){
-//     puntos = puntosStorage;
-// }
-
-// localStorage.setItem('puntos', puntos);
-
-
-const titulo = document.getElementById("titleColor");
+const title = document.getElementById("titleColor");
 const colors = document.getElementById("colors");
 const header = document.getElementById("header");
 const statusText = document.getElementById("status");
-const buttons = document.querySelectorAll("#button")
+const buttons = document.querySelectorAll("#button");
+let colorArray = [];
+let winner;
+let winnerColor;
+let timesWon = 0;
 
 function random() {
-    return Math.floor(Math.random()*(255 + 1));
+  return Math.floor(Math.random() * (255 + 1));
 }
 
-function randomColor(){
-    return `rgb(${random()}, ${random()}, ${random()})`
+function randomColor() {
+  return `rgb(${random()}, ${random()}, ${random()})`;
 }
 
-console.log(titulo)
-
-// Borrar todo
+// Delete all childs from parent element
 
 function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
 
-// Crear colores
+// Create color squares
 
 function createColor() {
-    let color = document.createElement("div");
-    color.classList.add('square')
-
-    color.style.backgroundColor = randomColor()
-    //crear
-    colors.appendChild(color);
+  let color = document.createElement("div");
+  color.classList.add("square");
+  color.style.backgroundColor = randomColor();
+  colors.appendChild(color);
 }
 
-let colorArray=[]
-let ganador
-let colorGanador
-let wintimes = 0
+/* Init function
+
+Removes all squares and creates new ones
+Asigns click listener for every square
+Chooses winner color
+
+*/
+
 function createGame(difficulty) {
-    removeAllChildNodes(colors)
-    header.style.backgroundColor = "#0A6245"
-    if (difficulty == "easy") {
-        for (let i = 0; i < 3; i++) {
-            createColor()
-        }
-    } else {
-        for (let i = 0; i < 6; i++) {
-            createColor()
-        }
+  removeAllChildNodes(colors);
+  header.style.backgroundColor = "#0A6245";
+  if (difficulty == "easy") {
+    for (let i = 0; i < 3; i++) {
+      createColor();
     }
-    colorArray = document.querySelectorAll(".square");
-    ganador = colorArray[Math.floor(Math.random()*colorArray.length)];
-    titulo.innerHTML = ganador.style.backgroundColor
-    colorGanador = ganador.style.backgroundColor
-    statusText.innerHTML = "ColorGame"
-    clickListener()
-    wintimes = 0
+  } else {
+    for (let i = 0; i < 6; i++) {
+      createColor();
+    }
+  }
+
+  colorArray = document.querySelectorAll(".square");
+  winner = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+  title.innerHTML = winner.style.backgroundColor;
+  winnerColor = winner.style.backgroundColor;
+
+  statusText.innerHTML = "ColorGame";
+  clickListener();
+  timesWon = 0;
 }
 
-createGame("hard")
+// Creates game on page load, hard by default.
+
+createGame("hard");
 
 function win() {
-    if (wintimes < 1) {
-        colorArray.forEach(function(i) {
-            i.style.backgroundColor = colorGanador
-        });
-        ganador.style.outline = "0.5rem solid"
-        buttons.forEach(i => {
-            i.classList.toggle("agrandar")
-            setTimeout(function () {
-                i.classList.toggle("agrandar")
-            },500);
-        });
-        header.style.backgroundColor = colorGanador
-        statusText.innerHTML = "Felicidades!"
-        // puntos = puntos + 10
-        // localStorage.setItem('puntos', puntos);  || En un futuro agregar sistema de puntos
-        wintimes++
-    }
-}
-function retry(item) {
-    statusText.innerHTML = "Intenta de nuevo!"
-    item.classList.toggle("shake")
-    setTimeout(function () {
-        item.classList.toggle("shake")
-    },500);
-    item.style.backgroundColor = "rgb(22, 22, 22)"
+
+  // If statment so you cant win multiple times after squares change to winner color
+  
+  if (timesWon < 1) {
+    colorArray.forEach(function (i) {
+      i.style.backgroundColor = winnerColor;
+    });
+
+    winner.style.outline = "0.5rem solid";
+
+    //Make buttons bigger on win
+
+    buttons.forEach((i) => {
+      i.classList.toggle("agrandar");
+      setTimeout(function () {
+        i.classList.toggle("agrandar");
+      }, 500);
+    });
+
+    header.style.backgroundColor = winnerColor;
+    statusText.innerHTML = "Felicidades!";
+
+    timesWon++;
+  }
 }
 
+function retry(item) {
+  statusText.innerHTML = "Intenta de nuevo!";
+  item.classList.toggle("shake");
+  setTimeout(function () {
+    item.classList.toggle("shake");
+  }, 500);
+  item.style.backgroundColor = "rgb(22, 22, 22)";
+}
+
+// Function to assing click listener to every square
+
 function clickListener() {
-    colorArray.forEach(function(i) {
-        i.addEventListener("click", function() {
-            let color = i.style.backgroundColor
-            if (color == colorGanador) {
-                win()
-            } else {
-                retry(i)
-            }
-        });
+  colorArray.forEach(function (i) {
+    i.addEventListener("click", function () {
+      let color = i.style.backgroundColor;
+      if (color == winnerColor) {
+        win();
+      } else {
+        retry(i);
+      }
     });
+  });
 }
